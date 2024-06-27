dceaed9109ad61dedab1cb4692dd725964644d5b
adarshares <adarshares@gmail.com>
2024-06-27 21:24:10 +0530

    different colours added for foreground and background, also text styles added

--- src/helper.rs
+++ src/helper.rs
@@ -1,18 +1,18 @@
 pub mod helpers {
     use std::fmt::Display;
 
-    pub fn apply_escape_sequence<T: Display>(content: T, converting_integer: i32) -> String {
+    fn apply_escape_sequence<T: Display>(content: T, converting_integer: i32) -> String {
         const ESCAPE_FRONT: &str = "\x1b[";
         const ESCAPE_SEQUENCE_END: &str = "m";
         const ESCAPE_LAST: &str = "0";
         let original_content = content.to_string();
 
-        return (String::from(
+        return String::from(
             String::from(
                 String::from(
                     String::from(
                         (String::from(
-                            String::from(ESCAPE_FRONT.clone())
+                            String::from(ESCAPE_FRONT)
                                 + converting_integer.to_string().as_str(),
                         )
                         .clone()
@@ -29,7 +29,126 @@ pub mod helpers {
                 + ESCAPE_LAST,
         )
         .clone()
-            + ESCAPE_SEQUENCE_END);
+            + ESCAPE_SEQUENCE_END;
     }
+
+
+    pub fn bold<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 1);
+    }
+    pub fn italic<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 3);
+    }
+    pub fn underline<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 4);
+    }
+    pub fn faint<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 2);
+    }
+    pub fn strike<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 9);
+    }
+    //foreground colors
+    pub fn black_front<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 30);
+    }
+    pub fn red_front<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 31);
+    }
+    pub fn green_front<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 32);
+    }
+    pub fn yellow_front<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 33);
+    }
+    pub fn blue_front<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 34);
+    }
+    pub fn magenta_front<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 35);
+    }
+    pub fn cyan_front<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 36);
+    }
+    pub fn white_front<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 37);
+    }
+    //background colors
+    pub fn black_back<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 40);
+    }
+    pub fn red_back<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 41);
+    }
+    pub fn green_back<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 42);
+    }
+    pub fn yellow_back<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 43);
+    }
+    pub fn blue_back<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 44);
+    }
+    pub fn magenta_back<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 45);
+    }
+    pub fn cyan_back<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 46);
+    }
+    pub fn white_back<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 47);
+    }
+
+    //foreground colors
+    pub fn bright_black_front<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 90);
+    }
+    pub fn bright_red_front<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 91);
+    }
+    pub fn bright_green_front<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 92);
+    }
+    pub fn bright_yellow_front<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 93);
+    }
+    pub fn bright_blue_front<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 94);
+    }
+    pub fn bright_magenta_front<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 95);
+    }
+    pub fn bright_cyan_front<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 96);
+    }
+    pub fn bright_white_front<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 97);
+    }
+    //background colors
+    pub fn bright_black_back<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 100);
+    }
+    pub fn bright_red_back<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 101);
+    }
+    pub fn bright_green_back<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 102);
+    }
+    pub fn bright_yellow_back<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 103);
+    }
+    pub fn bright_blue_back<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 104);
+    }
+    pub fn bright_magenta_back<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 105);
+    }
+    pub fn bright_cyan_back<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 106);
+    }
+    pub fn bright_white_back<T: Display>(content: T) -> String {
+        return apply_escape_sequence(content, 107);
+    }
+
 
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,241 @@
-#![allow(warnings)]
 pub mod helper;
+
+pub trait TextStyler {
+    //styles
+    fn bold(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::bold(self);
+    }
+    fn italic(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::italic(self);
+    }
+    fn underline(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::underline(self);
+    }
+    fn faint(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::faint(self);
+    }
+    fn strike(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::strike(self);
+    }
+    //foreground colors
+    fn black_front(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::black_front(self);
+    }
+    fn red_front(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::red_front(self);
+    }
+    fn green_front(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::green_front(self);
+    }
+    fn yellow_front(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::yellow_front(self);
+    }
+    fn blue_front(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::blue_front(self);
+    }
+    fn magenta_front(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::magenta_front(self);
+    }
+    fn cyan_front(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::cyan_front(self);
+    }
+    fn white_front(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::white_front(self);
+    }
+
+    //background colors
+    fn black_back(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::black_back(self);
+    }
+    fn red_back(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::red_back(self);
+    }
+    fn green_back(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::green_back(self);
+    }
+    fn yellow_back(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::yellow_back(self);
+    }
+    fn blue_back(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::blue_back(self);
+    }
+    fn magenta_back(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::magenta_back(self);
+    }
+    fn cyan_back(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::cyan_back(self);
+    }
+    fn white_back(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::white_back(self);
+    }
+
+    //bright colors
+    //foreground colors
+    fn bright_black_front(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::bright_black_front(self);
+    }
+    fn bright_red_front(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::bright_red_front(self);
+    }
+    fn bright_green_front(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::bright_green_front(self);
+    }
+    fn bright_yellow_front(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::bright_yellow_front(self);
+    }
+    fn bright_blue_front(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::bright_blue_front(self);
+    }
+    fn bright_magenta_front(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::bright_magenta_front(self);
+    }
+    fn bright_cyan_front(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::bright_cyan_front(self);
+    }
+    fn bright_white_front(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::bright_white_front(self);
+    }
+
+    //background colors
+    fn bright_black_back(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::bright_black_back(self);
+    }
+    fn bright_red_back(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::bright_red_back(self);
+    }
+    fn bright_green_back(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::bright_green_back(self);
+    }
+    fn bright_yellow_back(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::bright_yellow_back(self);
+    }
+    fn bright_blue_back(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::bright_blue_back(self);
+    }
+    fn bright_magenta_back(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::bright_magenta_back(self);
+    }
+    fn bright_cyan_back(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::bright_cyan_back(self);
+    }
+    fn bright_white_back(&self) -> String
+    where
+        Self: std::fmt::Display,
+    {
+        return helper::helpers::bright_white_back(self);
+    }
+}
+
+impl TextStyler for str{}
+impl TextStyler for String{}
 
 
 
@@ -9,6 +245,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        println!("{}",helper::helpers::apply_escape_sequence("random string",31));
+        println!("{}","random string".red_front().underline().strike());
     }
 }
